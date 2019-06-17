47dbf8b411de9893c7b508f05e48bfce53950b37
Coursera Learner <nobody@nowhere.nul>
2019-06-17 16:31:04 +0000

    Did assignment 7

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -9,22 +9,26 @@
 
 typedef struct _retire_info retire_info;
 
-  void retirement(int starAge,double initial,retire_info working,retire_info retired)
-  {
-    int i;
-    for(i=starAge;i<starAge+working.months;i++)
-      {
-	printf("Age %3d month %2d you have $%.2lf\n",i/12,i%12,initial);
-	initial = initial + initial*working.rate_of_return + working.contribution;
+void retirement (int startAge,   //in months
+		 double initial, //initial savings in dollars
+		 retire_info working, //info about working
+		 retire_info retired) //info about being retired
+{
+  int i;
+  for(i=startAge;i<startAge+working.months;i++)
+    {
+      printf("Age %3d month %2d you have $%.2lf\n",i/12,i%12,initial);
+      initial = initial + initial*working.rate_of_return + working.contribution;
 
-      }
-      for(i=starAge+working.months;i<starAge+working.months+retired.months;i++)
-	{
-	  printf("Age %3d month %2d you have $%.2lf\n",i/12,i%12,initial);
-	  initial = initial + initial*retired.rate_of_return + retired.contribution;
+    }
+  for(i=startAge+working.months;i<startAge+working.months+retired.months;i++)
+    {
+      printf("Age %3d month %2d you have $%.2lf\n",i/12,i%12,initial);
+      initial = initial + initial*retired.rate_of_return + retired.contribution;
 
-	}
-      }
+    }
+}
+
 
   int main()
   {
@@ -40,6 +44,6 @@ typedef struct _retire_info retire_info;
     b.months = 384;
     b.contribution = -4000;
     b.rate_of_return = 0.000833333333333333333;
-   retirement(age,money,a,b);
+    retirement(age,money,a,b);
     return EXIT_SUCCESS;
   }
