ca947c172cb2f292a61c3c970b7671a19285ac5c
Coursera Learner <nobody@nowhere.nul>
2019-06-27 15:25:42 +0000

    Did assignment 15

--- 15_tests_subseq/test-subseq.c
+++ 15_tests_subseq/test-subseq.c
@@ -24,9 +24,35 @@ int main (){
   ans2=maxSeq(ar,4);
   if (ans2 != 3) return EXIT_FAILURE;
 
-  int ar1[]={10,9,7,6};
-  ans2=maxSeq(ar1,4);
+  int ar6[]={10,9,7,6};
+  ans2=maxSeq(ar6,4);
   if (ans2 != 1) return EXIT_FAILURE;
+
+  int ar7[]={1,5,3,6,4,7,2,9,8};
+  ans2=maxSeq(ar7,9);
+  if (ans2 != 2) return EXIT_FAILURE;
+
+  int ar8[]={1};
+  ans2=maxSeq(ar8,1);
+  if (ans2 != 1) return EXIT_FAILURE;
+
+  int ar9[]={1,5,3,6,4,7,2,9,8};
+  ans2=maxSeq(ar9,0);
+  if (ans2 != 0) return EXIT_FAILURE;
+
+  int ar10[]={-5,-4,-3,-2,-1,0};
+  ans2=maxSeq(ar10,6);
+  if (ans2 != 6) return EXIT_FAILURE;
+
+  int ar11[]={22,44,55,11,99,101,1001,100001,1,2,670,33};
+  ans2=maxSeq(ar11,12);
+  if (ans2 != 5) return EXIT_FAILURE;
+
+  int ar12[]={1,3,5,4,9};
+  ans2=maxSeq(ar12,5);
+  if (ans2 != 3) return EXIT_FAILURE;
+
+
 
   return EXIT_SUCCESS;
 
