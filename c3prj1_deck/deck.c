147090dc12b1c89e4759b1819db51837175439c5
Coursera Learner <nobody@nowhere.nul>
2019-07-04 10:59:26 +0000

    Did assignment c3prj1_deck

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "deck.h"
-
+#include "cards.h"
 int com (card_t c1,card_t c2){
   if ((c1.value == c2.value) &&(c1.suit == c2.suit)) return 1;
   return 0;
