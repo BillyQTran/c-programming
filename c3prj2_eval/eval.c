bb32bc50b9abb4debbbe26e4b00aa0190795845b
Coursera Learner <nobody@nowhere.nul>
2019-07-05 08:05:51 +0000

    Did assignment c3prj2_eval

--- c3prj2_eval/eval.c
+++ c3prj2_eval/eval.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+
 int com1(card_t c1,card_t c2){
   if (c1.value == c2.value) return 1;
   return 0;
 }
-
 
 int card_ptr_comp(const void * vp1, const void * vp2) {
   const card_t * const * cp1=vp1;
@@ -181,6 +181,7 @@ int compare_hands(deck_t * hand1, deck_t * hand2) {
 //other functions we have provided can make
 //use of get_match_counts.
 
+/*
 unsigned * get_match_counts(deck_t * hand) {
   unsigned* arr=malloc(hand->n_cards*sizeof(*arr));
   for(int i=0 ; i< hand->n_cards ; i++){
@@ -192,7 +193,8 @@ unsigned * get_match_counts(deck_t * hand) {
     arr[i] = count;}
   return arr;
 }
-
+*/
+unsigned * get_match_counts(deck_t * hand) ;
 
 // We provide the below functions.  You do NOT need to modify them
 // In fact, you should not modify them!
