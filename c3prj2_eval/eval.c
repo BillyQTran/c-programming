f083f8d2b90ad66adc3b37b5a6c2a2489938ed9b
Coursera Learner <nobody@nowhere.nul>
2019-07-07 14:48:31 +0000

    Did assignment c3prj2_eval

--- c3prj2_eval/eval.c
+++ c3prj2_eval/eval.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-
+#include "cards.h"
+#include "deck.h"
 int com1(card_t c1,card_t c2){
-  if (c1.value == c2.value) return 1;
+  if ((c1.value == c2.value) &&(c1.suit == c2.suit)) return 1;
   return 0;
 }
 
